03edee0efd4c97b1c17d00bfb74e2cff1eb7975a
Talita Aya <tatakuriki@gmail.com>
2023-06-11 15:41:32 -0300

    edit and delete de usuário no bd

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,7 +4,26 @@ const getUser = async (req, res) => {
   try {
     const userList = await userModel.findAll()
     console.log(userList)
-    res.send('GET user')
+    res.send('Lista de usuários gerada')
+  } catch(error) {
+    return res.status(500).json({mensagem: error.message})
+  }
+}
+
+const getUserID = async (req, res) => {
+  try {
+    const {id} = req.params
+    const userID = await userModel.findOne({
+      where: {
+        id
+      }
+    })
+
+    if(!userID) {
+      return res.status(404).json({mensagem: "Este usuário não existe"})
+    }
+
+    res.status(200).json(userID)
   } catch(error) {
     return res.status(500).json({mensagem: error.message})
   }
@@ -23,10 +42,45 @@ const postUser = async(req, res) => {
     })
   
     console.log(newUser)
-    res.send('POST user')
+    res.send('Usuário criado')
   } catch (error) {
     return res.status(500).json({mensagem: error.message})
   }
 }
 
-module.exports = {getUser, postUser}
+const editUser = async (req, res) => {
+  try {
+    const {id} = req.params
+    const {name, age, email, username, password} = req.body
+  
+    const putUser = await userModel.findByPk(id)
+    putUser.name = name
+    putUser.age = age
+    putUser.email = email
+    putUser.username = username
+    putUser.password = password
+  
+    await putUser.save() //salvando no bd
+  
+    res.status(200).json({mensagem: putUser})
+  } catch (error) {
+    return res.status(500).json({mensagem: error.message})
+  }
+}
+
+const deleteUser = async (req, res) => {
+  try {
+    const {id} = req.params
+    await userModel.destroy({
+      where: {
+        id
+      }
+    })
+  
+    res.sendStatus(204)
+  } catch (error) {
+    return res.status(500).json({mensagem: error.message})
+  }
+}
+
+module.exports = {getUser, getUserID, postUser, editUser, deleteUser }
--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,10 +1,13 @@
 const express = require('express')
 
-var {getUser, postUser} = require('../controllers/user_controller')
+var {getUser, getUserID, postUser, editUser, deleteUser} = require('../controllers/user_controller')
 
 const router = express.Router()
 
 router.get('/users', getUser)
+router.get('/users/:id', getUserID)
 router.post('/users', postUser)
+router.put('/users/:id', editUser)
+router.delete('/users/:id', deleteUser)
 
 module.exports = router
